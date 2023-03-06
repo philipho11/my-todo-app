9b1fa30bfb42e015479d552910a13d8d61c5ce3a
philip ho <philipho11@gmail.com>
2023-03-06 16:49:33 +0800

    Add comment to bonus4

--- pymega/bonus4.py
+++ pymega/bonus4.py
@@ -1,5 +1,6 @@
 filenames = ["1.Raw Data.txt", "2.Reports.txt", "3.Presentations.txt"]
 
+#adding comment
 for filename in filenames:
     filename = filename.replace('.', '-', 1)
     print (filename)
